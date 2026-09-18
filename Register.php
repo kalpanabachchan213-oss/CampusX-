c68cd57e0562bfdf914070d9decdb450b8e5608c
Kalpana yadav <kalpanabachchan213@gmail.com>
2026-09-18 19:49:10 +0530

    Create registration form in Register.php

--- Register.php
+++ Register.php
@@ -1,1 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CampusX - Register</title>
+</head>
 
+<body>
+
+    <h1>CampusX ✅</h1>
+    <h2>Create Student Account</h2>
+
+    <form action="" method="POST">
+
+        <label>Full Name:</label><br>
+        <input type="text" name="name" required>
+
+        <br><br>
+
+        <label>Email:</label><br>
+        <input type="email" name="email" required>
+
+        <br><br>
+
+        <label>Password:</label><br>
+        <input type="password" name="password" required>
+
+        <br><br>
+
+        <label>Confirm Password:</label><br>
+        <input type="password" name="confirm_password" required>
+
+        <br><br>
+
+        <button type="submit">Register</button>
+
+    </form>
+
+    <p>
+        Already have an account?
+        <a href="login.php">Login here</a>
+    </p>
+
+</body>
+</html>
